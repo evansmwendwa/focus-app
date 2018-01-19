3bb3944517e7d1047fb3cfa0bb827fe02a4e4d4e
Evans Mwendwa <evansmwendwa@live.com>
2018-01-19 17:25:40 +0300

    allow toggling wallpaper widget

--- app/src/main/java/com/avm/evansmwendwa/focus/MainActivity.java
+++ app/src/main/java/com/avm/evansmwendwa/focus/MainActivity.java
@@ -11,30 +11,57 @@ public class MainActivity extends AppCompatActivity {
 
     private Boolean backgroundActive = false;
 
+    private Switch switchButton;
+    private TextView wallpaperText;
+    private LinearLayout wallpaperWidget;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        final Switch switchButton = (Switch) findViewById(R.id.switchWallpaper);
-        final TextView wallpaperText = (TextView) findViewById(R.id.wallpaperText);
+        // bind controls
+        switchButton = (Switch) findViewById(R.id.switchWallpaper);
+        wallpaperText = (TextView) findViewById(R.id.wallpaperText);
+        wallpaperWidget = (LinearLayout) findViewById(R.id.wallpaperWidget);
 
-        final LinearLayout wallpaperWidget = (LinearLayout) findViewById(R.id.wallpaperWidget);
+        // bind events
         wallpaperWidget.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                if(backgroundActive) {
-                    wallpaperWidget.setBackground(getResources().getDrawable(R.drawable.wallpaper_disabled));
-                    backgroundActive = false;
-                    switchButton.setChecked(false);
-                    wallpaperText.setTextColor(getResources().getColor(R.color.inactiveText));
-                } else {
-                    wallpaperWidget.setBackground(getResources().getDrawable(R.drawable.wallpaper_active));
-                    backgroundActive = true;
-                    switchButton.setChecked(true);
-                    wallpaperText.setTextColor(getResources().getColor(R.color.colorWhite));
-                }
+                toggleWallpaper();
             }
         });
+
+        switchButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                toggleWallpaper();
+            }
+        });
+    }
+
+    public void toggleWallpaper() {
+        if(this.backgroundActive) {
+            deactivateWallpaperWidget();
+            backgroundActive = false;
+        } else {
+            activateWallpaperWidget();
+            backgroundActive = true;
+        }
+    }
+
+    public void activateWallpaperWidget() {
+        wallpaperWidget.setBackground(getResources().getDrawable(R.drawable.wallpaper_active));
+        backgroundActive = true;
+        switchButton.setChecked(true);
+        wallpaperText.setTextColor(getResources().getColor(R.color.colorWhite));
+    }
+
+    public void deactivateWallpaperWidget() {
+        wallpaperWidget.setBackground(getResources().getDrawable(R.drawable.wallpaper_disabled));
+        backgroundActive = false;
+        switchButton.setChecked(false);
+        wallpaperText.setTextColor(getResources().getColor(R.color.inactiveText));
     }
 }
